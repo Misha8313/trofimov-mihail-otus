3cb89c70f117e7d9aaa4d9df5191828d3d766b44
misha8313 <misha8313@yandex.ru>
2021-08-08 23:57:02 +0300

    javascript homework 7. GraphQL schema

---
+++ javascript-7/src/data/customers.js
@@ -0,0 +1,14 @@
+const Customers = [
+  {
+    id: 1,
+    fio: 'Иванов Иван Иванович',
+    mail: 'mail1@mail.ru'
+  },
+  {
+    id: 2,
+    fio: 'Петров Петр Петрович ',
+    mail: 'mail1@mail.ru'
+  }
+];
+
+module.exports = Customers;
---
+++ javascript-7/src/data/products.js
@@ -0,0 +1,22 @@
+const Products = [
+  {
+    id: 1,
+    name: 'Computer',
+    price: 20,
+    count: 2
+  },
+  {
+    id: 2,
+    name: 'Monitor',
+    price: 30,
+    count: 3
+  },
+  {
+    id: 3,
+    name: 'Keyboard',
+    price: 40,
+    count: 4
+  }
+];
+
+module.exports = Products;
